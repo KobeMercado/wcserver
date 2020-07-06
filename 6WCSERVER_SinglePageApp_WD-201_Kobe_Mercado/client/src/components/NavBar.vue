227ab68f5387bf7a80b7ad66ce4270451fff01a4
KobeMercado <kobebryanmercado@gmail.com>
2020-07-06 17:48:32 +0800

    First commit

---
+++ 6WCSERVER_SinglePageApp_WD-201_Kobe_Mercado/client/src/components/NavBar.vue
@@ -0,0 +1,14 @@
+
+<template>
+<div>
+  <nav class="navbar">
+      <a href="#"><img src="images/logo.jpg" alt="logo" id="logo"></a>
+      <a href="/" id="b">Kofi.</a>
+      <a href="#merch" id="b">Merchandise</a>
+      <a href="#about" id="b">About Us</a>
+      <a href="#contact" id="b">Contact Us</a>
+</nav>
+
+</div>
+  
+</template>
---
+++ 6WCSERVER_SinglePageApp_WD-201_Kobe_Mercado/client/src/views/Home.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="Coffee">
+        <div id="home">
+            <section id="left">
+                <h1>Feel the cup <br> of Emotion</h1>
+                
+                <br>
+                <h6 >Looking for Coffee?</h6>
+                <h6>  We Grow, Process, Roast And Export High-Quality Coffee Worldwide.</h6>
+                <h6>Get Quality Product from us.</h6>
+                <br> <br>
+                <button id="order-button">Place Order</button>
+            </section>
+            <section id="right">
+                <img src="images/home-image.jpeg" alt="" id="home-image">
+            </section>
+        </div>
+
+        <div id="merch">
+            <section>
+                <h1>What's Hot?</h1>
+            </section>
+            <table>
+                <td>
+                    <tr>    
+                        <div>
+                            <img src="images/coffee-cup (6).jpeg" alt=""> 
+                            <div> 
+                                <span id="name" >Macchiato</span>
+                            </div>
+                        </div>  
+                    </tr>
+                </td>
+                <td>
+                    <tr>    
+                        <div>
+                            <img src="images/coffee-cup (2).jpeg" alt=""> 
+                            <div> 
+                                <span id="name" >Americano</span>
+                            </div>
+                        </div>  
+                    </tr>
+                </td>
+                <td>
+                    <tr>    
+                        <div>
+                            <img src="images/coffee-cup (3).jpeg" alt=""> 
+                            <div> 
+                                <span id="name" >Espresso</span>
+                            </div>
+                        </div>  
+                    </tr>
+                </td>
+            </table>
+        </div>
+
+        <div id="about">
+            <div id="about-text">
+                <h1>Made with Love to Serve You</h1>
+                <section>
+                    Brewing only the finest beans to deliver you warmth that will soothe.
+                    &copy;Kofi. , together with its trusted partners, guarantee a coffee experience like no other.
+                    Ranging from mild thirst-quenching drinks to strong vigor-giving roast, &copy; Kofi. serves it all.
+                    <br> <br>
+                    Brewing only the finest beans to deliver you warmth that will soothe.
+                    &copy;Kofi. , together with its trusted partners, guarantee a coffee experience like no other.
+                    Ranging from mild thirst-quenching drinks to strong vigor-giving roast, &copy; Kofi. serves it all.
+                </section>
+            </div>
+            <div id="img1">
+                <img src="images/about.jpeg" alt="">
+            </div>
+            <div id="img2">
+                <img src="images/about2.jpeg" alt="">
+            </div>
+
+        </div>
+
+        <div id="contact">
+            <img src="images/more-beans.jpeg" alt="" id="bg">
+            <div id="form">
+                <h5 style="color: rgb(184, 144, 14)">Enter your Details</h5>
+                <h1>Get in Touch</h1>
+                <form action="">
+                    <input type="text" name="name" placeholder="Name" id="input"><br><br>
+                    <input type="email" name="email" placeholder="Email" id="input"><br><br>
+                    <div>
+                    <textarea name="suggestion" id="suggestion" cols="40" rows="5" placeholder="Message"></textarea>
+                    </div>
+                </form>
+            </div>
+
+            <div id="info">
+                
+                795 South Park Avenue,<br> Melbourne, Australia
+                <br><br>
+                +91 12345 12345 <br>
+                +91 12345 12345
+                <br><br>
+                info@domainname.com <br>
+                sales@domainname.com
+            </div>
+        </div>
+
+        <footer id="footer">
+            <div>
+
+            </div>
+            <div id="footerlogo">
+                <img src="images/logo.jpg" alt="" id="logo">
+                <p>&copy;kofishop2020</p>
+            </div>
+            
+        </footer>
+    </div>
+
+</template>
